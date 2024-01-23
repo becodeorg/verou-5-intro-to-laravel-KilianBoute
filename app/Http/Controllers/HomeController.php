b4ed5128d1cc5511fb936b160cd963ed4c60c4f3
Kilian Boute <kilian.boute@gmail.com>
2024-01-23 14:35:47 +0100

    Add laravel; pages, dynamic links

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view("welcome", ['greeting' => 'Hi']);
+    }
+
+    function showPage($slug)
+    {
+        return view($slug);
+    }
+}
